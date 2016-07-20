0101a3d0413e8a5ed71259aaac2829080e8c7011
tsmliu213 <tsmliu213@gmail.com>
2016-07-19 23:02:06 -0700

    anagram solution

--- CCTC_Practice/src/Array_and_strings.java
+++ CCTC_Practice/src/Array_and_strings.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class Array_and_strings {
 	Array_and_strings() {
@@ -7,6 +9,8 @@ public class Array_and_strings {
 	public void run() {
 		System.out.println(uniqueChar("test"));
 		System.out.println(uniqueChar("abcdefgh"));
+		System.out.println(isAnagram("hello", "lleoh"));
+		System.out.println(isAnagram("hello", "test"));
 	}
 	
 	// Implement an algorithm to determine if a string has all unique characters 
@@ -21,6 +25,16 @@ public class Array_and_strings {
 			charSet[val] = true;
 		}
 		return true;
+	}
+	
+	private boolean isAnagram(String str1, String str2) {
+		char[] c_str1 = str1.toCharArray();
+		char[] c_str2 = str2.toCharArray();
+		Arrays.sort(c_str1);
+		Arrays.sort(c_str2);
+		String str1_sorted = new String(c_str1);
+		String str2_sorted = new String(c_str2);
+		return str1_sorted.equals(str2_sorted);
 	}
 	
 }
