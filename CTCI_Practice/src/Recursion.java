6b9913f60e53cee88ad272f830e7aa710733790f
tsmliu213 <tsmliu213@gmail.com>
2016-08-03 22:51:05 -0700

    recursion update

--- CTCI_Practice/src/Recursion.java
+++ CTCI_Practice/src/Recursion.java
@@ -24,4 +24,27 @@ public class Recursion {
 			
 		}
 	}
+	
+	// Calulate the number of ways to represent cents
+	private int numOfChange(int amount, int coin){
+		int nextCoin = 0;
+		switch(coin){
+			case 25:
+				nextCoin = 10;
+				break;
+			case 10:
+				nextCoin = 5;
+				break;
+			case 5:
+				nextCoin = 1;
+				break;
+			case 1:
+				return 1;
+		}
+		int ways = 0;
+		for(int i = 0; i * coin < amount; i++) {
+			ways += numOfChange(amount - i * coin, nextCoin);
+		}
+		return ways;
+	}
 }
